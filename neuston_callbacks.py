21fd5b469b648b7b2f64e5dcfa159a1ef9484862
Paul Covert <82682319+paul-covert@users.noreply.github.com>
2025-06-24 17:46:55 -0700

    Updated to PyTorch 2.x api

--- neuston_callbacks.py
+++ neuston_callbacks.py
@@ -17,7 +17,8 @@ from neuston_data import IfcbBinDataset
 
 ## Training ##
 
-class SaveValidationResults(ptl.callbacks.base.Callback):
+#class SaveValidationResults(ptl.callbacks.base.Callback): # deprecated in PyTorch 2.x
+class SaveValidationResults(ptl.callbacks.callback.Callback):
 
     def __init__(self, outdir, outfile, series, best_only=True):
         self.outdir = outdir
@@ -272,8 +273,9 @@ def _save_run_results(outfile, results):
     if outfile.endswith('.h5'): _save_run_results_hdf(outfile, results)
 
 
-class SaveTestResults(ptl.callbacks.base.Callback):
-
+#class SaveTestResults(ptl.callbacks.base.Callback): # deprecated in PyTorch 2.x
+class SaveTestResults(ptl.callbacks.callback.Callback):
+    
     def __init__(self, outdir, outfile, timestamp):
         self.outdir = outdir
         self.outfile = outfile
